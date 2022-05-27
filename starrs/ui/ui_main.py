8e29c63925466937444d8927fc544c96b7f1a8c1
Kiryha <kiryha@gmail.com>
2022-05-26 20:18:53 -0400

    add add student

--- starrs/ui/ui_main.py
+++ starrs/ui/ui_main.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'E:\projects\master\CIS556\dev\starrs\ui\ui_main.ui'
 #
-# Created: Thu May 26 19:26:34 2022
+# Created: Thu May 26 20:17:49 2022
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,7 +12,7 @@ from PySide import QtCore, QtGui
 class Ui_STARRS(object):
     def setupUi(self, STARRS):
         STARRS.setObjectName("STARRS")
-        STARRS.resize(1118, 718)
+        STARRS.resize(563, 877)
         self.centralwidget = QtGui.QWidget(STARRS)
         self.centralwidget.setObjectName("centralwidget")
         self.verticalLayout = QtGui.QVBoxLayout(self.centralwidget)
@@ -21,9 +21,32 @@ class Ui_STARRS(object):
         self.tabSections.setObjectName("tabSections")
         self.tab_1 = QtGui.QWidget()
         self.tab_1.setObjectName("tab_1")
+        self.verticalLayout_2 = QtGui.QVBoxLayout(self.tab_1)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
         self.tabStudents = QtGui.QTableView(self.tab_1)
-        self.tabStudents.setGeometry(QtCore.QRect(9, 9, 431, 621))
         self.tabStudents.setObjectName("tabStudents")
+        self.verticalLayout_2.addWidget(self.tabStudents)
+        self.splitter_2 = QtGui.QSplitter(self.tab_1)
+        self.splitter_2.setMaximumSize(QtCore.QSize(16777215, 55))
+        self.splitter_2.setOrientation(QtCore.Qt.Vertical)
+        self.splitter_2.setObjectName("splitter_2")
+        self.splitter = QtGui.QSplitter(self.splitter_2)
+        self.splitter.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.splitter.setOrientation(QtCore.Qt.Horizontal)
+        self.splitter.setObjectName("splitter")
+        self.linStudentFirstName = QtGui.QLineEdit(self.splitter)
+        self.linStudentFirstName.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.linStudentFirstName.setObjectName("linStudentFirstName")
+        self.linStudentLastName = QtGui.QLineEdit(self.splitter)
+        self.linStudentLastName.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.linStudentLastName.setObjectName("linStudentLastName")
+        self.linStudentDescription = QtGui.QLineEdit(self.splitter)
+        self.linStudentDescription.setMaximumSize(QtCore.QSize(16777215, 20))
+        self.linStudentDescription.setObjectName("linStudentDescription")
+        self.pushButton = QtGui.QPushButton(self.splitter_2)
+        self.pushButton.setMaximumSize(QtCore.QSize(16777215, 30))
+        self.pushButton.setObjectName("pushButton")
+        self.verticalLayout_2.addWidget(self.splitter_2)
         self.tabSections.addTab(self.tab_1, "")
         self.tab_2 = QtGui.QWidget()
         self.tab_2.setObjectName("tab_2")
@@ -37,7 +60,7 @@ class Ui_STARRS(object):
         self.verticalLayout.addWidget(self.tabSections)
         STARRS.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(STARRS)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1118, 21))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 563, 21))
         self.menubar.setObjectName("menubar")
         self.menuEdit = QtGui.QMenu(self.menubar)
         self.menuEdit.setObjectName("menuEdit")
@@ -56,6 +79,7 @@ class Ui_STARRS(object):
 
     def retranslateUi(self, STARRS):
         STARRS.setWindowTitle(QtGui.QApplication.translate("STARRS", "MainWindow", None, QtGui.QApplication.UnicodeUTF8))
+        self.pushButton.setText(QtGui.QApplication.translate("STARRS", "Add Student", None, QtGui.QApplication.UnicodeUTF8))
         self.tabSections.setTabText(self.tabSections.indexOf(self.tab_1), QtGui.QApplication.translate("STARRS", "Online Application Process", None, QtGui.QApplication.UnicodeUTF8))
         self.tabSections.setTabText(self.tabSections.indexOf(self.tab_2), QtGui.QApplication.translate("STARRS", "Admissions Process", None, QtGui.QApplication.UnicodeUTF8))
         self.tabSections.setTabText(self.tabSections.indexOf(self.tab_3), QtGui.QApplication.translate("STARRS", "Online Registration", None, QtGui.QApplication.UnicodeUTF8))
